49aec7d5ec8d96775b3a429e068cfb7c930a1fa9
Tanish <64203208+tanish14@users.noreply.github.com>
2021-02-08 16:55:21 +0530

    Update pixelSetter.js

--- src/util/pixelSetter.js
+++ src/util/pixelSetter.js
@@ -1,4 +1,10 @@
-/** Sets the pixels from 0 through length of value. */
+/**  
+ * @param {number} x x-coordinate.
+ * @param {number} y y-coordinate.
+ * @param {object} value
+ * @param {number} pixel
+ * @description Sets the pixels from 0 through length of value. 
+*/
 module.exports = function(x, y, value, pixels){
   for(let i = 0; i < value.length; i++){
     pixels.set(x, y, i, value[i]);
